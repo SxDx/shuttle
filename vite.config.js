51789e0336eb6d28da243f1e4a6035f8443e00c6
René Koller <r.koller@me.com>
2021-08-15 14:54:52 +0200

    update config for gitlab pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/shuttle/",
-  // build: {
-  //   outDir: "public",
-  // },
+  base: "/shuttle/",
   plugins: [vue()],
 });
