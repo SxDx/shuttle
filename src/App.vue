911933e663ea54526ce59118d5cec47aa3b86deb
René Koller <r.koller@me.com>
2023-01-20 17:37:10 +0100

    Update dependencies and sites

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,47 @@
 <template>
-
-
   <div
-    class="
-      flex
-      justify-center
-      items-center
-      w-screen
-      h-screen
-      font-mono
-      relative
-    "
+    class="flex justify-center items-center w-screen h-screen font-mono relative"
   >
-
-
     <div
       id="blob1"
-      class="
-        absolute
-        animate-blob
-        filter
-        blur-xl
-        opacity-50
-        w-96
-        h-96
-        rounded-full
-        left-[45%]
-        top-[25%]
-      "
+      class="absolute animate-blob filter blur-xl opacity-50 w-96 h-96 rounded-full left-[45%] top-[25%]"
     ></div>
-
 
     <div
       id="blob2"
-      class="
-        absolute
-        animate-blob
-        animation-delay-2000
-        filter
-        blur-xl
-        opacity-50
-        w-96
-        h-96
-        rounded-full
-        right-[45%]
-        top-[25%]
-      "
+      class="absolute animate-blob animation-delay-2000 filter blur-xl opacity-50 w-96 h-96 rounded-full right-[45%] top-[25%]"
     ></div>
-
 
     <div
       id="blob3"
-      class="
-        absolute
-        animate-blob
-        animation-delay-4000
-        filter
-        blur-xl
-        opacity-50
-        w-96
-        h-96
-        rounded-full
-        right-[30%]
-        top-[45%]
-      "
+      class="absolute animate-blob animation-delay-4000 filter blur-xl opacity-50 w-96 h-96 rounded-full right-[30%] top-[45%]"
     ></div>
 
-
     <div
-      class="
-        bg-white/70
-        dark:bg-primary-400/90
-        rounded-2xl
-        shadow-xl
-        w-5/6
-        lg:w-2/3
-        flex
-        overflow-hidden
-        relative
-      "
+      class="bg-white/70 dark:bg-primary-400/90 rounded-2xl shadow-xl w-5/6 lg:w-2/3 flex overflow-hidden relative"
     >
-
-
       <div class="hidden md:block w-0 md:w-4/12 relative">
-
-
         <img src="./assets/bg.gif" role="presentation" />
 
-
         <weather />
-
-
       </div>
-
 
       <div class="w-full lg:w-8/12 p-8 flex flex-col">
-
-
         <div class="flex items-center flex-grow-1 h-full backdrop-blur-xl">
-
-
           <div
-            class="
-              grid grid-cols-2
-              lg:grid-cols-3
-              w-full
-              gap-y-5
-              lg:gap-y-11
-              gap-x-2
-            "
+            class="grid grid-cols-2 lg:grid-cols-3 w-full gap-y-5 lg:gap-y-11 gap-x-2"
           >
-
-
             <div v-for="link in links" :key="link.name">
-
-
               <page :config="link" />
-
-
             </div>
-
-
           </div>
-
-
         </div>
-
-
       </div>
-
-
     </div>
 
-
     <mode-switch />
-
-
   </div>
-
-
 </template>
-
 
 <script setup>
 import Page from "./components/Page.vue";
@@ -154,19 +55,9 @@ const links = [
     color: "#FFFFFF",
   },
   {
-    name: "Google Cloud",
-    url: "https://console.cloud.google.com",
-    color: "#1A73E8",
-  },
-  {
     name: "Hacker News",
-    url: "https://vue-hn.herokuapp.com/top",
+    url: "news.ycombinator.com",
     color: "#CD201F",
-  },
-  {
-    name: "Product Hunt",
-    url: "https://www.producthunt.com",
-    color: "#DA552F",
   },
   {
     name: "The Guardian",
@@ -184,13 +75,12 @@ const links = [
     color: "#E2127A",
   },
   {
-    name: "Stratechery",
-    url: "https://stratechery.com",
-    color: "#FAA633",
+    name: "Nautilus",
+    url: "https://nautil.us/",
+    color: "#FFD01A",
   },
 ];
 </script>
-
 
 <style scoped>
 #blob1 {
@@ -203,5 +93,3 @@ const links = [
   background-color: #19385c;
 }
 </style>
-
-
